33ab51de2d85d946ece4b9b320c61fcc2a6f6278
tonyng <tonyng987@gmail.com>
2019-11-25 15:07:57 -0800

    Add user routes

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const User = require("./models/User");
+const users = require("./routes/api/users");
+const passport = require("passport");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to mongoDB"))
   .catch(err => console.log(error));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
