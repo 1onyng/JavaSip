e144fb3186020df5e25bef453d6058ff23b31379
RyanGonzalezUSA <mhalim80@hotmail.com>
2019-11-25 15:53:48 -0800

    add authentication components

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>JavaSip</h1>
+        <footer>
+          <a href="https://github.com/1onyng">Tony</a>
+          <a href="https://github.com/tmockler22">Tristan</a>
+          <a href="https://github.com/RyanGonzalezUSA">Mohamed</a>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
