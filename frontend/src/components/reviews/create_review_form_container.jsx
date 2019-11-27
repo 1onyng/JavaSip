f53beaeac1368f74512012d0b9a1a54c5bdcb13e
RyanGonzalezUSA <mhalim80@hotmail.com>
2019-11-27 13:08:46 -0800

    add receive reviews

---
+++ frontend/src/components/reviews/create_review_form_container.jsx
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+// TODO to be updated from tony work
+import { createReview } from '../../actions/business_actions';
+import ReviewForm from './review_form';
+
+const mSTP = () => ({
+  review: { rate: 5, comment: '' },
+  formType: "Create Review"
+});
+
+const mDTP = dispatch => ({
+  submitEvent: event => dispatch(createEvent(event))
+});
+
+export default connect(mSTP, mDTP)(ReviewForm);
