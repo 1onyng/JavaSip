246f49163f5a0c9b346873ae3e849418fe90ad9e
Tristan <tmocklercoding@gmail.com>
2019-11-26 19:07:20 -0600

    Add style to logout button

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -17,7 +17,7 @@ class LoginForm extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.currentUser === true) {
-      this.props.history.push('/ShopsForm');
+      this.props.history.push('/');
     }
 
     this.setState({errors: nextProps.errors})
@@ -37,7 +37,7 @@ class LoginForm extends React.Component {
       password: this.state.password
     };
     this.props.login(user)
-    .then(()=> this.props.history.push('/ShopsForm'))
+    .then(()=> this.props.history.push('/'))
   }
 
   renderErrors() {
