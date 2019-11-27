507c4c92a94cf33ff66bb737bfb6a126d0d9f315
tonyng <tonyng987@gmail.com>
2019-11-27 12:55:59 -0800

    Add business#show

--- frontend/src/util/business_api_util.js
+++ frontend/src/util/business_api_util.js
@@ -1,17 +1,5 @@
 import axios from 'axios';
 
-export const getBusinesses = () => {
-  return axios.get('/api/businesses')
-};
-
-export const getUserBusinesses = id => {
-  return axios.get(`/api/businesses/user/${id}`)
-};
-
-export const writeBusiness = data => {
-  return axios.post('/api/businesses/', data)
-};
-
-export const reviewBusiness = (businessId, comment, rate) => {
-  return axios.post('/api/businesses/id/review', {businessId: businessId, comment: comment, rate: rate})
+export const fetchBusiness = id => {
+  return axios.get(`/api/businesses/${id}`)
 };
