1201c3dd6c62c312c5429800e22670bca1509fc3
tonyng <tonyng987@gmail.com>
2019-11-27 16:32:37 -0800

    Add reviews to business#show

--- frontend/src/actions/business_actions.js
+++ frontend/src/actions/business_actions.js
@@ -3,15 +3,15 @@ import * as BusinessApiUtil from "../util/business_api_util";
 export const RECEIVE_BUSINESS = "RECEIVE_BUSINESS";
 export const RECEIVE_BUSINESS_REVIEWS = "RECEIVE_BUSINESS_REVIEWS";
 
-const receiveBusiness = ({ business }) => {
+const receiveBusiness = (payload) => {
   return {
     type: RECEIVE_BUSINESS,
-    business: business,
-    // reviews: business.reviews
+    payload
   };
 };
 
 export const fetchBusiness = id => dispatch => {
   return BusinessApiUtil.fetchBusiness(id)
-    .then(payload => dispatch(receiveBusiness(payload.data)));
+    .then(payload => {      
+      dispatch(receiveBusiness(payload.data))})
 };
