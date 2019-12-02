f2a55537f3c118776aaadfaa4e3a794531fbb52b
tonyng <tonyng987@gmail.com>
2019-12-02 15:53:40 -0800

    Update business show render

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -8,18 +8,25 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import BusinessShowContainer from "./businesses/business_show_container";
 import BusinessSearchContainer from "./businesses/business_search_container";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faLaptop, faSearch, faStar, faMapMarkerAlt, faPhone, faWindowRestore, faCamera, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
-const App = () => (
+const App = () => {
+  library.add(fab, faLaptop, faSearch, faStar, faMapMarkerAlt, faPhone, faWindowRestore, faCamera, faTrashAlt);
+  return(
   <div>
-    <Switch>
-        <Route exact path="/" component={MainPage}/>
-        <AuthRoute exact path="/login" component={LoginFormContainer} />
-        <AuthRoute exact path="/signup" component={SignupFormContainer} />
-        <AuthRoute exact path="/ShopsForm" component={ShopsFormContainer} />
-        <Route exact path ='/businesses/:businessId' component={BusinessShowContainer}/>
-        <Route exact path='/businesses/search' component={BusinessSearchContainer} />  
-    </Switch>
-  </div>
-);
+      <Switch>
+          <Route exact path="/" component={MainPage}/>
+          <AuthRoute exact path="/login" component={LoginFormContainer} />
+          <AuthRoute exact path="/signup" component={SignupFormContainer} />
+          <AuthRoute exact path="/ShopsForm" component={ShopsFormContainer} />
+          <Route exact path ='/businesses/:businessId' component={BusinessShowContainer}/>
+          <Route exact path='/businesses/search' component={BusinessSearchContainer} />  
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
--- frontend/src/components/businesses/business_show_container.js
+++ frontend/src/components/businesses/business_show_container.js
@@ -1,12 +1,14 @@
 import { fetchBusiness } from "../../actions/business_actions";
 import { fetchUsers } from "../../actions/session_actions";
+import { deleteReview } from "../../actions/reviews_actions";
 import BusinessShow from "./business_show";
 import { connect } from "react-redux";
 
 const mapStateToProps = (state, ownProps) => {
+
   const businessId = (ownProps.match.params.businessId);
   const business = state.entities.businesses[businessId]; 
-  const currentUser = state.session.currentUser;
+  const currentUser = state.session.user;
   const reviews = Object.keys(state.entities.reviews).map(id => state.entities.reviews[id]);
   const users = state.entities.users;
 
@@ -22,7 +24,8 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = dispatch => {
   return {
     fetchBusiness: id => dispatch(fetchBusiness(id)),
-    fetchUsers: () => dispatch(fetchUsers())
+    fetchUsers: () => dispatch(fetchUsers()),
+    removeReview: reviewId => dispatch(deleteReview(reviewId))
   };
 };
 
