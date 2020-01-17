7533e3249274bfdd6bdfd5b405d8b5087dcd3543
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-01-17 12:31:33 -0800

    hide keys

--- config/keys_prod.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY,
-  AWSAccessKeyID: process.env.AWS_ACCESS_KEY_ID,
-  AWSSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  AWSRegion: process.env.AWS_REGION,
-  AWSBucket: process.env.AWS_BUCKET
-};
