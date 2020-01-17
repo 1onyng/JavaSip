44a6da38032933bf7ee486cccc0f8c29f10dfc05
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-01-17 12:31:18 -0800

    hide keys

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://dev:Et9LWuUata81Ya6u@cluster0-vyimk.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "Kw4:yn$yiP0<k9V",
-  AWSAccessKeyID: 'AKIAIMNTKMXM3EWXNOOQ',
-  AWSSecretAccessKey: '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',
-  AWSRegion: 'us-west-1',
-  AWSBucket: 'javasip-bucket',
-};
