ada321e0165e567b39d861c28b0fe936738fd1fa
tonyng <tonyng987@gmail.com>
2020-01-24 08:22:41 -0800

    Update login path

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (decodedUser.exp < currentTime) {
       store.dispatch(logout());
-      window.location.href = "/login";
+      window.location.href = "/#/login";
     }
   } else {
     store = configureStore({});
