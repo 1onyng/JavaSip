43d38f72128aacaad9863be83231bfe2116e6b94
tonyng <tonyng987@gmail.com>
2020-02-02 10:12:51 -0800

    Allow enter key to submit search

--- frontend/src/components/nav/searchbar.js
+++ frontend/src/components/nav/searchbar.js
@@ -7,25 +7,42 @@ class SearchBar extends React.Component {
     this.state = {
       location: '',
     };
+    this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  update(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
+  handleSubmit(event) {
+    event.preventDefault();
+    const location = this.state.location;
+    if (this.props.history.location.pathname !== "/search") {
+      this.props.history.push({
+        pathname: "/search",
+        search: location,
+      });
+    }
+    else {
+      let arr = location.split(" ");
+      let str = arr.join("%20")
+      this.props.history.push(`/search?${str}`)
+    }
+  }
+
+  update(e) {
+    this.setState({ location: e.target.value });
   }
 
   render() {
     return (
-        <div className="search-bar">
-            <p className="search-bar-how">Coffee Shops In</p>
-            <input type="text" className="search-input"
-              value={this.state.location}
-              onChange={this.update('location')}
-              placeholder="Oakland, San Francisco, or San Jose" />
-
-            <Link to={`/search/${this.state.location}`}><button className="search-bar-submit"><i className="fas fa-search"></i></button></Link>
-          </div>
+      <form onSubmit={this.handleSubmit}>
+        <div className="search-bar">     
+          <p className="search-bar-how">Coffee Shops In</p>
+          <input type="text" className="search-input"
+            value={this.state.location}
+            onChange={this.update}
+            placeholder="Oakland, San Francisco, or San Jose" />
+          <Link to={`/search/${this.state.location}`}><button className="search-bar-submit"><i className="fas fa-search"></i></button></Link>
+        </div>
+      </form >
     );
   }
 }
